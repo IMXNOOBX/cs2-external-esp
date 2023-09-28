3666813673ca000ed51e838c23f1b8115ca2f411
IMXNOOBX <paraelmobil403@gmail.com>
2023-09-28 18:51:03 +0200

    Added an auto updater, and updated to the latest game version

--- memory-external/classes/config.hpp
+++ memory-external/classes/config.hpp
@@ -6,13 +6,13 @@ namespace config {
 	extern bool read();
 	extern void save();
 
-	inline std::ptrdiff_t dwLocalPlayer = 0x17153E8;
-	inline std::ptrdiff_t dwEntityList = 0x167BCC8;
-	inline std::ptrdiff_t dwViewMatrix = 0x1715D90;
+	inline std::ptrdiff_t dwLocalPlayer = 0x17DB108;
+	inline std::ptrdiff_t dwEntityList = 0x178C888;
+	inline std::ptrdiff_t dwViewMatrix = 0x187A6E0;
 
 	inline std::ptrdiff_t dwPawnHealth = 0x808;
-	inline std::ptrdiff_t dwPlayerPawn = 0x5dc;
+	inline std::ptrdiff_t dwPlayerPawn = 0x7FC;
 	inline std::ptrdiff_t dwSanitizedName = 0x720;
 	inline std::ptrdiff_t m_iTeamNum = 0x3bf;
-	inline std::ptrdiff_t m_vecOrigin = 0x12AC;
+	inline std::ptrdiff_t m_vecOrigin = 0x1204;
 }
