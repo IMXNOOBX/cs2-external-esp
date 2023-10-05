2eeaa152453c43581b653daf30c38bbec10bdbbe
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2023-10-05 16:56:12 +0200

    Added unknowncheats build configuration without auto updater

--- memory-external/classes/auto_updater.hpp
+++ memory-external/classes/auto_updater.hpp
@@ -17,9 +17,11 @@ namespace fs = std::filesystem;
 namespace updater {
 	const std::string file_path = "offsets.json";
 
+#ifndef _UC
 	bool check_and_update(bool prompt_update);
 	bool get_last_commit_date(json& commit);
 	bool download_file(const char* url, const char* localPath);
+#endif
 	bool file_good(const std::string& name);
 
 	extern bool read();
@@ -29,14 +31,14 @@ namespace updater {
 	inline std::string raw_updated_offets = "https://github.com/IMXNOOBX/cs2-external-esp/raw/main/offsets/offsets.json";
 
 	namespace offsets {
-		inline std::ptrdiff_t dwLocalPlayer = 0x187AC28;
-		inline std::ptrdiff_t dwEntityList = 0x178D8C8;
-		inline std::ptrdiff_t dwViewMatrix = 0x187B710;
+		inline std::ptrdiff_t dwLocalPlayer = 0x187AC48;
+		inline std::ptrdiff_t dwEntityList = 0x178D8E8;
+		inline std::ptrdiff_t dwViewMatrix = 0x187B730;
 
 		inline std::ptrdiff_t dwPawnHealth = 0x808;
 		inline std::ptrdiff_t dwPlayerPawn = 0x7FC;
 		inline std::ptrdiff_t dwSanitizedName = 0x720;
-		inline std::ptrdiff_t m_iTeamNum = 0x3bf;
-		inline std::ptrdiff_t m_vecOrigin = 0x1204;
+		inline std::ptrdiff_t m_iTeamNum = 0x3BF;
+		inline std::ptrdiff_t m_vecOrigin = 0x1214;
 	}
 }
