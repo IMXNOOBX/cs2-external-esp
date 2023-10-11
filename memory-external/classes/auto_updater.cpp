3bffab794be59e8ef0fe1b59e1fce3cc30bbfde0
ifBars <114284668+ifBars@users.noreply.github.com>
2023-10-11 04:49:11 -0600

    Update auto_updater.cpp
    - Added toggleable head tracker

--- memory-external/classes/auto_updater.cpp
+++ memory-external/classes/auto_updater.cpp
@@ -193,6 +193,8 @@ namespace updater {
 		if (data["dwViewMatrix"].is_number())
 			offsets::dwViewMatrix = data["dwViewMatrix"];
 
+		if (data["pClippingWeapon"].is_number())
+			offsets::pClippingWeapon = data["pClippingWeapon"];
 		if (data["m_iPawnArmor"].is_number())
 			offsets::m_iPawnArmor = data["m_iPawnArmor"];
 		if (data["dwPawnHealth"].is_number())
@@ -216,6 +218,7 @@ namespace updater {
 		data["dwEntityList"] = offsets::dwEntityList;
 		data["dwViewMatrix"] = offsets::dwViewMatrix;
 
+		data["pClippingWeapon"] = offsets::pClippingWeapon;
 		data["m_iPawnArmor"] = offsets::m_iPawnArmor;
 		data["dwPawnHealth"] = offsets::dwPawnHealth;
 		data["dwPlayerPawn"] = offsets::dwPlayerPawn;
