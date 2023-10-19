0eec7ed1d6ee50cc9ec15e8d4ab5cce007b3aaad
ifBars <114284668+ifBars@users.noreply.github.com>
2023-10-19 14:31:54 -0600

    Update auto_updater.hpp

--- memory-external/classes/auto_updater.hpp
+++ memory-external/classes/auto_updater.hpp
@@ -1,43 +1,11 @@
-#pragma once
-#include <Windows.h>
-#include <WinINet.h>
-#include <iostream>
-#include <fstream>
-#include <ctime>
-#include <string>
-#include <vector>
-#include <sstream>
-#include <filesystem>
-#include "json.hpp"
-
-using json = nlohmann::json;
-namespace fs = std::filesystem;
-
-
-namespace updater {
-	const std::string file_path = "offsets.json";
-
-#ifndef _UC
-	bool check_and_update(bool prompt_update);
-	bool get_last_commit_date(json& commit);
-	bool download_file(const char* url, const char* localPath);
-#endif
-	bool file_good(const std::string& name);
-
-	extern bool read();
-	extern void save();
-
-	const inline std::string github_repo_api = "https://api.github.com/repos/IMXNOOBX/cs2-external-esp/commits";
-	const inline std::string raw_updated_offets = "https://github.com/IMXNOOBX/cs2-external-esp/raw/main/offsets/offsets.json";
-
-	inline int build_number = 13963;
-
-	namespace offsets {
-		inline std::ptrdiff_t dwLocalPlayer = 0x17E27C8;
+inline std::ptrdiff_t dwLocalPlayer = 0x17E27C8;
 		inline std::ptrdiff_t dwEntityList = 0x1793DC8;
 		inline std::ptrdiff_t dwViewMatrix = 0x1881D70;
 		inline std::ptrdiff_t dwBuildNumber = 0x487514;
+		inline std::ptrdiff_t dwPlantedC4 = 0x1887FC0;
 
+		inline std::ptrdiff_t m_vecAbsOrigin = 0xC8;
+		inline std::ptrdiff_t m_pGameSceneNode = 0x310;
 		inline std::ptrdiff_t m_bIsDefusing = 0x1390;
 		inline std::ptrdiff_t m_szName = 0xC18;
 		inline std::ptrdiff_t m_pClippingWeapon = 0x1290;
@@ -47,5 +15,3 @@ namespace updater {
 		inline std::ptrdiff_t dwSanitizedName = 0x720;
 		inline std::ptrdiff_t m_iTeamNum = 0x3BF;
 		inline std::ptrdiff_t m_vecOrigin = 0x1214;
-	}
-}
