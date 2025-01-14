69e555747d414ea4d8b65b9412780627e1dc8f4e
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2025-01-14 15:18:53 +0100

    Added title feature status display, few config tweaks, changed default value of offsets to 0 to verify if they have been correctly parsed, added title update on feature changes & Updated gitignore

---
+++ memory-external/classes/utils.cpp
@@ -0,0 +1,14 @@
+﻿#include "utils.h"
+
+void Utils::update_console_title() {
+	std::string title = "cs2-external-esp";
+
+    config::show_box_esp ? title += " | Box: [O]" : title += " | Box: [X]";
+    config::show_skeleton_esp ? title += " | Skeleton: [O]" : title += " | Skeleton: [X]";
+    config::show_head_tracker ? title += " | Head Tracker: [O]" : title += " | Head Tracker: [X]";
+    config::team_esp ? title += " | Team: [O]" : title += " | Team: [X]";
+    config::automatic_update ? title += " | Auto Update: [O]" : title += " | Auto Update: [X]";
+    config::show_extra_flags ? title += " | Flags: [O]" : title += " | Flags: [X]";
+
+	SetConsoleTitle(title.c_str());
+}
---
+++ memory-external/classes/utils.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <windows.h>
+#include <string>
+
+#include "config.hpp"
+
+class Utils {
+private:
+public:
+	void update_console_title();
+};
+
+inline Utils utils;
