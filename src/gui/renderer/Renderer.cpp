db6b2977f67c8752eefc9b798874296d297aa5fa
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2026-07-25 13:06:02 +0200

    Exported IsFocused function from Renderer

--- src/gui/renderer/Renderer.cpp
+++ src/gui/renderer/Renderer.cpp
@@ -23,6 +23,10 @@ bool Renderer::IsOpen() {
     return GetInstance().isOpen;
 }
 
+bool Renderer::IsFocused() {
+    return GetInstance().isFocused;
+}
+
 bool Renderer::InitImpl() {
     if (!Window::SpawnWindow()) {
         LOGF(FATAL, "Failed to create window");
